ecb7a1ff74da3c87e38bc1b6c03efc77e7466ab6
swatantra-on-the-way <swatantrachhetri3@gmail.com>
2024-08-08 23:07:58 +1000

    Add final styling

--- js-file.js
+++ js-file.js
@@ -26,7 +26,7 @@ function GameBoard(){
     const refreshBoard = () => {
         for (let i = 0; i < rows; i++){
             for (let j = 0; j < columns; j++){
-                board[i][j].addToken(0);
+                board[i][j].addToken("");
             }
         }
     }
@@ -49,6 +49,7 @@ function Cell(){
 function GameController(playerOne = "Player 1",  playerTwo = "Player 2"){
     const board = GameBoard();
     let gameOver = false;
+    let gameTie = false;
     const players = [
         {name: playerOne, token: "X"}, {name: playerTwo, token: "O"}
     ];
@@ -76,14 +77,17 @@ function GameController(playerOne = "Player 1",  playerTwo = "Player 2"){
         activePlayer = players[0];
     }
 
+    const resetGameFlags = () => {
+        gameOver = false;
+        gameTie = false;
+    }
+
     const playRound = (rowIndex, colIndex) => {
         if (gameOver){
             return;
         }
         let currPlayer = activePlayer.token;
         board.dropToken(rowIndex, colIndex, currPlayer);
-
-        let gameTie = false;
         //Check for vertical-win
         board.getBoard().reduce((ver_count, row) => {
             if (row[colIndex].getValue() === currPlayer)
@@ -98,7 +102,7 @@ function GameController(playerOne = "Player 1",  playerTwo = "Player 2"){
 
         //Check for horizontal-win
        board.getBoard().forEach((row, index) => {
-        if (index === rowIndex){
+        if ((index + "") === rowIndex){
             row.reduce((hor_count, cell) => {
                 if (cell.getValue() === currPlayer){
                     if (hor_count === 2){
@@ -126,11 +130,11 @@ function GameController(playerOne = "Player 1",  playerTwo = "Player 2"){
         //Check for game-tie
         let count = 0;
         board.getBoard().forEach((row) => row.forEach((cell) => {
-            if (cell.getValue() !== ""){
+            if (cell.getValue() === ""){
                 count++;
             }
         }));
-        if (count === 9){
+        if (count === 0 && !gameOver){
             gameTie = true;
             gameOver = true;
         }
@@ -144,8 +148,6 @@ function GameController(playerOne = "Player 1",  playerTwo = "Player 2"){
             else{
                 console.log(`${activePlayer.name} won this round`);
             }
-            // board.refreshBoard();
-            resetActivePlayer();
         }
         else {
             switchPlayer();
@@ -153,53 +155,117 @@ function GameController(playerOne = "Player 1",  playerTwo = "Player 2"){
         printNewRound();
     }
 
+    const isGameOver = () => gameOver;
+    const isGameTied = () => gameTie;
+
     printNewRound();
 
     return {
         playRound,
         getActivePlayer,
-        getBoard: board.getBoard
+        getBoard: board.getBoard,
+        isGameOver,
+        isGameTied,
+        resetActivePlayer,
+        resetGameFlags,
+        refreshBoard: board.refreshBoard
     }
 }
 
-function ScreenController(){
-    const game = GameController("Ram", "Shyam");
+function ScreenController(player1, player2){
+    const game = GameController(player1, player2);
+    const body = document.querySelector("body");
+    const gameContainer = document.querySelector(".container");
     const boardContainer = document.querySelector('.board');
-
     const clickEventHandler = (e) => {
-        if (!(e.target.dataset.column && e.target.dataset.row)){
+        if (!e.target.dataset.row || e.target.dataset.isClicked || game.isGameOver()){
             return;
         }
+        e.target.dataset.isClicked = true;
         let rowIndex = e.target.dataset.row;
         let colIndex = e.target.dataset.column;
+        let player = game.getActivePlayer();
         game.playRound(rowIndex, colIndex);
-        updateScreen();
+        updateScreen(e, player);
     }
 
-    const updateScreen = () => {
+    const startScreen = () => {
+        body.classList.add("body-background");
+        gameContainer.style.display = "flex";
+        playerTurn.textContent = `${game.getActivePlayer().name}'s turn`;
         let board = game.getBoard();
         boardContainer.textContent = "";
         board.map((row, rowIndex) => row.map((col, colIndex) => {
             let cell = document.createElement("button");
             cell.dataset.column = colIndex;
             cell.dataset.row = rowIndex;
+            cell.dataset.isClick = false;
             cell.textContent = col.getValue();
             cell.classList.add("cell");
             boardContainer.appendChild(cell);
         }))
-        let playerTurn = document.querySelector(".turn");
-        playerTurn.textContent = `${game.getActivePlayer().name}'s turn`;
-    };
-    boardContainer.addEventListener("click", clickEventHandler);
-    updateScreen();
+    }
 
+    const playAgain = () => {
+        game.resetActivePlayer();
+        game.refreshBoard();
+        game.resetGameFlags();
+        result.style.display = "none";
+        startScreen();
+    }
+
+    const playerTurn = document.querySelector(".turn");
+    const result = document.querySelector(".result");
+    const r1 = document.querySelector(".result p:nth-child(1)");
+    const r2 = document.querySelector(".result p:nth-child(2)");
+    const playAgainBtn = document.querySelector("#play-again");
+
+    const updateScreen = ((event, player) => {
+        if (player.token === "X"){
+            event.target.style.backgroundColor = "#be185d";
+        }
+        else {
+            event.target.style.backgroundColor = "#6d28d9";
+        }
+        event.target.textContent = `${player.token}`;
+        playerTurn.textContent = `${game.getActivePlayer().name}'s turn`;
+        //Result Screen
+        if (game.isGameOver()){
+            r1.textContent = `Game Over !!!`;
+            if (!game.isGameTied()){
+                r2.textContent = `${player.name} won`;
+            }
+            else {
+                r2.textContent = `It's a tie`;
+            }
+            result.style.display = "flex";
+        }
+    });
+    boardContainer.addEventListener("click", clickEventHandler);
+    playAgainBtn.addEventListener("click", playAgain);
+    startScreen();
 }
 
-let display = ScreenController();
-// let game = GameController("Ram", "Shyam");
-// game.playRound(0, 0);
-// game.playRound(1, 0);
-// game.playRound(0, 1);
-// game.playRound(1, 1)
-// game.playRound(0, 2);
-// game.playRound(1, 2);
+
+function IntroScreen(){
+    //Form details extraction
+    const form = document.querySelector("#intro-screen");
+    const p1 = document.querySelector("#player1-name");
+    const p2 = document.querySelector("#player2-name");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        form.style.display = "none";
+        let player1Name = p1.value;
+        let player2Name = p2.value;
+
+        if (player1Name === ""){
+            player1Name = undefined;
+        }
+        if (player2Name === ""){
+            player2Name = undefined;
+        }
+        ScreenController(player1Name, player2Name);
+    });     
+}
+
+let start = IntroScreen();
